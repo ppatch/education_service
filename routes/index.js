1b4d913080925fae54bff68c12d87f14e9932016
ppatch <605351841@qq.com>
2015-08-14 15:11:16 +0800

    wx14

--- routes/index.js
+++ routes/index.js
@@ -271,7 +271,8 @@ router.post('/liuyan', function (req, res, next) {
 //读取留言
 router.get('/message', function (req, res, next) {
     console.log(req.query.openid)
-    Message.find(req.query)
+
+    Message.find({openid:req.query.openid})
         .sort({date: -1}).exec(function (error, result) {
             if(error!=null){next(error)}
             else {
